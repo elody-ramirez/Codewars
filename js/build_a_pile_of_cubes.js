7d8b43a606c7e0df51a4ac4b1a74b5de243e5a12
elody-ramirez <elody.ramirez@gmail.com>
2020-03-31 03:26:40 -0400

    Set up build a pile of cubes problem with prompt, declare function & and test cases

---
+++ js/build_a_pile_of_cubes.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/* Your task is to construct a building which will be a pile of n cubes. The
+cube at the bottom will have a volume of n^3, the cube above will have volume of
+(n-1)^3 and so on until the top which will have a volume of 1^3.
+
+You are given the total volume m of the building. Being given m can you find
+the number n of cubes you will have to build?
+
+The parameter of the function findNb (find_nb, find-nb, findNb) will be an
+integer m and you have to return the integer n such as
+n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n. */
+
+function findNb(m) {
+    // your code
+    return (-1);
+}
+
+findNb(4183059834009) // Should equal 2022
+findNb(24723578342962) // Should equal -1
+findNb(135440716410000) // Should equal 4824
+findNb(40539911473216) // Should equal 3568
