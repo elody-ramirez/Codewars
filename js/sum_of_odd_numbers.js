55474628d1c209027a876df1d2d3424a9b6f114e
elody-ramirez <elody.ramirez@gmail.com>
2020-04-01 06:37:49 -0400

    Put prompt, function name, and test cases for sum of odd numbers problem

---
+++ js/sum_of_odd_numbers.js
@@ -0,0 +1,18 @@
+'use strict'
+
+/* Given the triangle of consecutive odd numbers:
+
+             1                            = 1
+          3     5                         = 8
+       7     9    11                      = 27
+   13    15    17    19                   = 64
+21    23    25    27    29                = 125
+...                                       = 216
+Calculate the row sums of this triangle from the row index (starting at index 1)*/
+
+function rowSumOddNumbers(n) {
+	// TODO
+}
+
+console.log(rowSumOddNumbers(1))
+console.log(rowSumOddNumbers(42))
