c92a9606a7e80f4b6433b0ce6485704ec39a5316
elody-ramirez <elody.ramirez@gmail.com>
2020-04-02 23:38:18 -0400

    Solved problem using regex and while statements

--- js/string_incrementer.js
+++ js/string_incrementer.js
@@ -23,12 +23,26 @@ Attention: If the number has leading zeros the amount of digits should be
 considered.*/
 
 function incrementString (strng) {
-  // return incrementedString
+  // First pull just the word
+  const onlyWord = strng.replace(/\d+/g, '')
+  // Second pull just the number
+  let num = strng.slice(onlyWord.length) || "0"
+  // then get the length of the number to be used to fill in 0s if necessary
+  const numbersLength = strng.length  - onlyWord.length
+  // Next lines, converts to int, increments 1 and then back to string
+  num = parseInt(num)
+  num++
+  num = num.toString()
+  // fills in 0s if needed
+  while (num.length < numbersLength) {
+    num = "0" + num
+  }
+  return onlyWord + num
 }
 
 console.log(incrementString("foobar000")) // "foobar001"
 console.log(incrementString("foo")) // "foo1"
-console.log(incrementString("foobar001")) // "foobar002"
+console.log(incrementString("fooobar001")) // "foobar002"
 console.log(incrementString("foobar99")) // "foobar100"
 console.log(incrementString("foobar099")) // "foobar100"
 console.log(incrementString("")) // "1"
