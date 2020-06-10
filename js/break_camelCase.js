d273f0d72ff4567ccbfcd94fb1ba71541f3537fe
elody-ramirez <elody.ramirez@gmail.com>
2020-06-09 22:27:52 -0400

    Optimized Solution using regex

--- js/break_camelCase.js
+++ js/break_camelCase.js
@@ -3,6 +3,11 @@ a space between words. */
 
 // complete the function
 function solution(string) {
+  return(string.replace(/([A-Z])/g, ' $1'));
+}
+
+// complete the function
+function solution2(string) {
   let answer = ""
   for (let i = 0; i < string.length; i++){
     if (string[i] === string[i].toUpperCase()) {
