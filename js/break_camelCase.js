b3befcbacfecfb35b9fc8925a94b513656480c40
elody-ramirez <elody.ramirez@gmail.com>
2020-06-09 22:21:15 -0400

    Prompt and example problems for Break Camel Case challenge

---
+++ js/break_camelCase.js
@@ -0,0 +1,10 @@
+/* Complete the solution so that the function will break up camel casing, using
+a space between words. */
+
+// complete the function
+function solution(string) {
+  
+}
+
+console.log(solution('camelCasing'))
+console.log(solution('camelCasingTest'))
