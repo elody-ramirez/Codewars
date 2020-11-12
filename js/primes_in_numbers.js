837792b90f659bdeeb388fe29fa1fd9d44dd388e
elody-ramirez <elody.ramirez@gmail.com>
2020-11-11 23:04:24 -0500

    Using the solution I arrived at in Python, I converted the syntax to JavaScript

--- js/primes_in_numbers.js
+++ js/primes_in_numbers.js
@@ -9,18 +9,21 @@ with the p(i) in increasing order and n(i) empty if n(i) is 1.
 Example: n = 86240 should return "(2**5)(5)(7**2)(11)" */
 
 function primeFactors(n){
-  return isPrime(n)
-}
-
-function isPrime(num){
-  for (let i = 2; i < num; i++){
-    if(num % i === 0) return false;
-  return num > 1;
+  let result = ""
+  let i = 1
+  while (n > 1) {
+    i += 1
+    let count = 0
+    while (n % i == 0){
+      n /= i
+      count += 1
+    }
+    if (count > 0) {
+      result += "("+i+"**"+count+")"
+    }
   }
+  result = result.replace("**1","")
+  return result
 }
 
 console.log(primeFactors(7775460)) // (2**2)(3**3)(5)(7)(11**2)(17)
-console.log(primeFactors(2))
-console.log(primeFactors(3))
-console.log(primeFactors(7))
-console.log(primeFactors(9))
