5c39e7de8012bae4557e4505bb91cfed08763fb4
elody-ramirez <elody.ramirez@gmail.com>
2020-11-11 20:46:15 -0500

    Prompt, function, test cases and expected results for Primes in Numbers challenge in Python

---
+++ py/primes_in_numbers.py
@@ -0,0 +1,18 @@
+# Given a positive number n > 1 find the prime factor decomposition of n.
+#The result will be a string with the following form :
+
+ #"(p1**n1)(p2**n2)...(pk**nk)"
+
+#where a ** b means a to the power of b
+#with the p(i) in increasing order and n(i) empty if n(i) is 1.
+
+#Example: n = 86240 should return "(2**5)(5)(7**2)(11)" */
+
+def prime_factors(n):
+    ...
+
+print(primeFactors(7775460)) #(2**2)(3**3)(5)(7)(11**2)(17)
+print(primeFactors(2))
+print(primeFactors(3))
+print(primeFactors(7))
+print(primeFactors(9))
