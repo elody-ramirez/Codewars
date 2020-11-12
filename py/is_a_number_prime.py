923c983e42ac8380b12be8236f5dc1e35042806a
elody-ramirez <elody.ramirez@gmail.com>
2020-11-12 12:48:28 -0500

    Brute force solution that runs from 2 until the sqrt of given number

--- py/is_a_number_prime.py
+++ py/is_a_number_prime.py
@@ -16,29 +16,38 @@
 # is_prime(1)  /* false */
 # is_prime(2)  /* true  */
 # is_prime(-1) /* false */
+import math
 
 def is_prime(num):
-  pass #TODO
+    result = True
+    if num > 2:
+        top = int(math.sqrt(num)) + 1
+        for i in range(2, top):
+            print(num, i, num%i)
+            if (num % i) == 0:
+                result = False
+    elif num < 2:
+        result = False
+    return result
 
-
-print(is_prime(0))    # False, "0  is not prime"
-print(is_prime(1))    # False, "1  is not prime"
-print(is_prime(2))    # True, "2  is prime"
-print(is_prime(73))   # True, "73 is prime"
-print(is_prime(75))   # False, "75 is not prime"
-print(is_prime(-1))   # False, "-1 is not prime"
-
-print(is_prime(3))    # True, "3  is prime"
-print(is_prime(5))    # True, "5  is prime"
-print(is_prime(7))    # True, "7  is prime"
-print(is_prime(41))   # True, "41 is prime"
-print(is_prime(5099)) # True, "5099 is prime"
+# print(is_prime(0))    # False, "0  is not prime"
+# print(is_prime(1))    # False, "1  is not prime"
+# print(is_prime(2))    # True, "2  is prime"
+# print(is_prime(73))   # True, "73 is prime"
+# print(is_prime(75))   # False, "75 is not prime"
+# print(is_prime(-1))   # False, "-1 is not prime"
+#
+# print(is_prime(3))    # True, "3  is prime"
+# print(is_prime(5))    # True, "5  is prime"
+# print(is_prime(7))    # True, "7  is prime"
+# print(is_prime(41))   # True, "41 is prime"
+# print(is_prime(5099)) # True, "5099 is prime"
 
 print(is_prime(4))    # False, "4  is not prime"
 print(is_prime(6))    # False, "6  is not prime"
 print(is_prime(8))    # False, "8  is not prime"
 print(is_prime(9))    # False, "9 is not prime"
-print(is_prime(45))   # False, "45 is not prime"
-print(is_prime(-5))   # False, "-5 is not prime"
-print(is_prime(-8))   # False, "-8 is not prime"
-print(is_prime(-41))  # False, "-41 is not prime"
+# print(is_prime(45))   # False, "45 is not prime"
+# print(is_prime(-5))   # False, "-5 is not prime"
+# print(is_prime(-8))   # False, "-8 is not prime"
+# print(is_prime(-41))  # False, "-41 is not prime"
