00f489d1265c22b4e269ff1f2b49055828b89cf4
elody-ramirez <elody.ramirez@gmail.com>
2020-11-13 13:12:18 -0500

    Brute force method with a nested loop and 2 if statements

--- py/write_number_in_expanded_form.py
+++ py/write_number_in_expanded_form.py
@@ -9,7 +9,19 @@
 # NOTE: All numbers will be whole numbers greater than 0.
 
 def expanded_form(num):
-    pass
+    result = ''
+    n = list(map(int, str(num)))
+    remainder = len(n)
+    for i in range(0, len(n)):
+        remainder -= 1
+        if n[i] != 0:
+            result+=str(n[i])
+            for i in range(0, remainder):
+                result+='0'
+            result += ' + '
+    if result[-3:] == ' + ':
+        result = result[:-3]
+    return result
 
 print(expanded_form(12))     # '10 + 2');
 print(expanded_form(42))     # '40 + 2');
