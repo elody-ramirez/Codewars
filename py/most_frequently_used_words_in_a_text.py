7adead13ab2bba02bc547b2d4263e0d46b59cffd
elody-ramirez <elody.ramirez@gmail.com>
2021-04-07 22:25:59 -0400

    Used Counter and regex modules in order to filter the text and count the occurence of each

--- py/most_frequently_used_words_in_a_text.py
+++ py/most_frequently_used_words_in_a_text.py
@@ -30,17 +30,24 @@
 # Avoid creating an array whose memory footprint is roughly as big as the input
 # text. Avoid sorting the entire array of unique words.
 
-def top_3_words(text):
-    return
+from collections import Counter
+import re
 
-top_3_words("a a a  b  c c  d d d d  e e e e e")) # "e", "d", "a"
-top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e")) # "e", "ddd", "aa"
-top_3_words("  //wont won't won't ")) # "won't", "wont"
-top_3_words("  , e   .. ")) # "e"
-top_3_words("  ...  ")) #
-top_3_words("  '  ")) #
-top_3_words("  '''  ")) #
-top_3_words("""In a village of La Mancha, the name of which I have no desire to
+def top_3_words(text):
+    words = re.findall("[\s]?([']?[A-Za-z]+'?[A-Za-z']*)[\s,:]?", text)
+    c = Counter()
+    for x in words:
+        c[x.lower()] += 1
+    return [x[0] for x in c.most_common(3)]
+
+print(top_3_words("a a a  b  c c  d d d d  e e e e e")) # "e", "d", "a"
+print(top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e")) # "e", "ddd", "aa"
+print(top_3_words("  //wont won't won't ")) # "won't", "wont"
+print(top_3_words("  , e   .. ")) # "e"
+print(top_3_words("  ...  ")) #
+print(top_3_words("  '  ")) #
+print(top_3_words("  '''  ")) #
+print(top_3_words("""In a village of La Mancha, the name of which I have no desire to
 call to mind, there lived not long since one of those gentlemen that keep a
 lance in the lance-rack, an old buckler, a lean hack, and a greyhound for
 coursing. An olla of rather more beef than mutton, a salad on most
