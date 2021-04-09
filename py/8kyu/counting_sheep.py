3b796854ed00f07fe517d5d0f2ebb5d74619419b
elody-ramirez <elody.ramirez@gmail.com>
2021-04-08 21:58:21 -0400

    Prompt, function, test cases and expected results in Python for Smallest Integer In Array

--- py/8kyu/counting_sheep.py
+++ py/8kyu/counting_sheep.py
@@ -26,10 +26,10 @@ def count_sheeps(sheep):
 #     return count
 
 array1 = [True,  True,  True,  False,
-          True,  True,  True,  True ,
+          True,  True,  True,  True,
           True,  False, True,  False,
-          True,  False, False, True ,
-          True,  True,  True,  True ,
+          True,  False, False, True,
+          True,  True,  True,  True,
           False, False, True,  True ];
 
 print(count_sheeps(array1)) # 17
